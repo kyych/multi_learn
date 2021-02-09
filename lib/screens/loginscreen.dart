2f8af918ec5f4ce6ea7e9e9489fadf4d1f65b6a0
kyych <m.biernat43@gmail.com>
2021-02-09 20:09:36 +0100

    Input fields and buttons in LoginBox added.

--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'dart:developer' as developer;
 
 import 'package:hexcolor/hexcolor.dart';
+import 'package:multi_learn/widgets/loginbox.dart';
 
 class LoginScreen extends StatefulWidget {
   _LoginScreenState createState() => _LoginScreenState();
@@ -36,17 +37,45 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
           Padding(
             padding: EdgeInsets.only(top: 175),
-            child: Container(
-              decoration: BoxDecoration(
-                color: HexColor('#8FB3f5'),
-                borderRadius: BorderRadius.all(Radius.circular(37)),
-              ),
-              height: 320,
-              width: 330,
-            ),
+            child: LoginBox(),
           ),
         ],
       ),
     ));
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: HexColor('#8FB3f5'),
+        borderRadius: BorderRadius.all(Radius.circular(37)),
+      ),
+      child: Column(
+        children: <Widget>[
+          Column(
+            children: <TextField>[
+              TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none, hintText: 'Login'),
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: InputBorder.none, hintText: 'Password'),
+              )
+            ],
+          ),
+        ],
+      ),
+      height: 320,
+      width: 330,
+    );
   }
 }
---
+++ lib/widgets/loginbox.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+import 'dart:developer' as developer;
+
+class LoginBox extends StatefulWidget {
+  _LoginBoxState createState() => _LoginBoxState();
+}
+
+class _LoginBoxState extends State<LoginBox> {
+  final loginTextController = TextEditingController();
+  final passwordTextController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    loginTextController.dispose();
+    passwordTextController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: HexColor('#8FB3f5'),
+        borderRadius: BorderRadius.all(Radius.circular(37)),
+      ),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              children: <TextField>[
+                TextField(
+                  controller: loginTextController,
+                  decoration: InputDecoration(
+                      border: InputBorder.none, hintText: 'Login'),
+                ),
+                TextField(
+                  controller: passwordTextController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: InputBorder.none, hintText: 'Password'),
+                )
+              ],
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              FlatButton(
+                onPressed: loginClicked,
+                child: Text('Login'),
+              ),
+              SizedBox(
+                width: 78,
+              ),
+              FlatButton(
+                child: Text('Signup'),
+              ),
+            ],
+          ),
+        ],
+      ),
+      height: 320,
+      width: 330,
+    );
+  }
+
+  void loginClicked() {
+    developer.log(loginTextController.text);
+  }
+}
