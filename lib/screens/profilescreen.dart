bdf1966acc72524a01023514423c845da3b49154
kyych <m.biernat43@gmail.com>
2021-02-16 23:30:09 +0100

    added signOut functionality in profileScreen

--- lib/screens/profilescreen.dart
+++ lib/screens/profilescreen.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:multi_learn/screens/loginscreen.dart';
 
 class ProfileScreen extends StatefulWidget {
   _ProfileScreen createState() => _ProfileScreen();
@@ -17,7 +19,28 @@ class _ProfileScreen extends State<ProfileScreen> {
         end: Alignment.bottomRight,
         colors: [HexColor("#3F497B"), HexColor("#7EAAD1")],
       )),
-      child: Center(child: Text('Profile')),
+      child: Column(
+        children: [
+          Center(child: Text('Profile')),
+          FlatButton(
+              textColor: Colors.amber,
+              child: Text('Logout'),
+              onPressed: () async {
+                // Navigator.of(context).pop();
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()),
+                    (route) => false);
+
+                await FirebaseAuth.instance.signOut();
+              }),
+        ],
+      ),
     ));
+  }
+
+  Future<void> logout() async {
+    print('LOGOUT CLICKED');
+    await FirebaseAuth.instance.signOut();
   }
 }
